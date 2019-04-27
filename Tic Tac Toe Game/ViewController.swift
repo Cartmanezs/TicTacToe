f2bd267bb47d73fe5d44aacfb31bf94769e6e612
Igor Palyvoda <tib.igor.mk@gmail.com>
2019-04-27 11:37:16 +0300

    Initial Commit

---
+++ Tic Tac Toe Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tic Tac Toe Game
+//
+//  Created by Ingvar on 4/27/19.
+//  Copyright © 2019 Igor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
